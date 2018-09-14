966625950b53b0ffeece001ee11e46e0c640a817
L_aZY <18846171630@163.com>
2018-09-14 16:58:34 +0800

    提交 单链表 代码

---
+++ list/src/linkedList/Test.java
@@ -0,0 +1,51 @@
+package linkedList;
+
+/**
+ * @author LaZY（李志一）
+ * @data 2018/9/14 - 16:57
+ */
+public class Test {
+    public static void main(String[] args) {
+        Singly_LinkedList s = new Singly_LinkedList();
+        /*
+         * s.initByInsertFromTill(10);// 尾插法创建单链表
+         * s.printSingly_LinkedList();//打印结果
+         */
+        /*
+         * s.initByInsertFromHead(10);// 头插法创建单链表
+         * s.printSingly_LinkedList();//打印结果
+         */
+
+        /*
+         * s.clearSingly_LinkedList();//整表删除法清空单链表
+         * s.printSingly_LinkedList();//打印结果
+         */
+        /*
+         * s.deleteSingly_linkedList();//暴力清空单链表
+         * s.printSingly_LinkedList();//打印结果
+         */
+
+        /*
+         * s.initByInsertFromTill(10);// 头插法创建单链表
+         * System.out.println(s.locateNode(111));//查找指定位置节点
+         */
+
+        /*
+         * s.initByInsertFromTill(10);// 头插法创建单链表 s.insertNode(10,
+         * newNodeBySingly_LinkedList("0s0"));//插入节点
+         * s.printSingly_LinkedList();//打印结果
+         */
+
+        /*
+         * s.initByInsertFromTill(10);// 头插法创建单链表 s.insert(10,
+         * newNodeBySingly_LinkedList("0s0"));//插入节点
+         * s.printSingly_LinkedList();//打印结果
+         */
+
+        /*
+         * s.initByInsertFromTill(10);// 头插法创建单链表
+         * s.delete(9);//删除节点
+         * s.printSingly_LinkedList();//打印结果
+         */
+    }
+}
